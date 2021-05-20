feea75068c0caeedfaa3499a0df843b51caf9823
Atharv-khandelwal <83112922+Atharv-khandelwal@users.noreply.github.com>
2021-05-20 18:21:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function bedroom(){
+    window.location="bedroom.html"
+}
+
+function tvAc(){
+    window.location="tvandac.html"
+}
+
+function basket(){
+    window.location="fruitbasket.html"
+}
+
+function bottles(){
+    window.location="bottles.html"
+}
+
+function desk(){
+    window.location="desk.html"
+}
